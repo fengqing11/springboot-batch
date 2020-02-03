10d11e9b622e911221e4094c0a1427d02bdf7fd7
fengqing11 <tanzi8845@gmail.com>
2020-02-03 22:28:32 +0800

    first commit

---
+++ readme.md
@@ -0,0 +1,11 @@
+# springboot-batch
+
+## 遇到的问题
+
+1.java.lang.IllegalArgumentException: The JobParameters must not be null.
+
+解决方法：尚无
+
+2.Caused by: com.mysql.cj.exceptions.InvalidConnectionAttributeException: The server time zone value '
+
+解决方法：https://blog.csdn.net/qq_33811662/article/details/80532699
---
+++ src/main/java/xyz/fengqing11/springbootbatch/SpringbootBatchApplication.java
@@ -0,0 +1,15 @@
+package xyz.fengqing11.springbootbatch;
+
+import org.springframework.batch.core.configuration.annotation.EnableBatchProcessing;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+@EnableBatchProcessing
+public class SpringbootBatchApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringbootBatchApplication.class, args);
+    }
+
+}
